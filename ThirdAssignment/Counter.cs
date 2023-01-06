9aa23aaf354df844beb62b58c07be30ff4fb1fa4
Sujithkharvi <Admin@DESKTOP-44DNH9H>
2023-01-06 11:04:46 +0530

    Add project files.

---
+++ ThirdAssignment/Counter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdAssignment
+{
+    public sealed class Counter
+    {
+        public static int countvalue;
+        private Counter()
+        {
+            countvalue = 0;
+        }
+
+        private static Counter instance = null;
+       
+        public static Counter GetInstance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new Counter();
+                }
+                return instance;
+            }
+        }
+
+        public int count
+        {
+            get
+            {
+                return countvalue;
+            }
+        }
+
+        public void increment()
+        {
+            countvalue++;
+        }
+        public void decrement()
+        {
+            countvalue--;
+        }
+    }
+}
---
+++ ThirdAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ThirdAssignment;
+
+
+Counter inst1 = Counter.GetInstance;
+Counter inst2 = Counter.GetInstance;
+
+inst1.increment();
+inst2.decrement();
+/*inst1.decrement();
+inst2.decrement();*/
+// because of singleton class, bothe inst1 and inst2 will refer to same memory location , both inst1 and inst2 are same
+int value = inst1.count;
+Console.WriteLine(value);
+
+
+
+
+
